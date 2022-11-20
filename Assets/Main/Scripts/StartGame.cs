58b7ae704ea9d3d8338ea7c950eb0fa062d18679
Andy Chen <44120813+achen5671@users.noreply.github.com>
2022-11-20 18:05:07 -0500

    High score, Player name input, End Scene(#6)
    * Added end scene
    * Save high score and name to scoreboard

--- Assets/Main/Scripts/StartGame.cs
+++ Assets/Main/Scripts/StartGame.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
+    [SerializeField] public InputField playerNameInput;
+
     // Start is called before the first frame update
     public void LoadGame(int index) {
+        string s = playerNameInput.text;
+        PersistentData.Instance.SetName(s);
         SceneManager.LoadScene(index);
-    }
+    }  
 }
